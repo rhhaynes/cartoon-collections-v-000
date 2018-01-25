17d9286811ba82b927ad91d3412f2a53cef0c676
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-25 14:37:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(arr)
   arr.map{|n| n.capitalize}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls()
+  
 end
 
 def find_the_cheese# code an argument here
