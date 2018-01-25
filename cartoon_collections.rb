5316f1e50ba66bd6f9e9b09f683f5d362925fc75
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-25 14:54:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def roll_call_dwarves(arr)
   arr.each_with_index{|n,i| puts "#{i+1}. #{n}"}
 end
@@ -13,6 +15,7 @@ end
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
   arr_cheese   = arr & cheese_types
+  binding.pry
   arr_cheese.empty? ? nil : arr_cheese[0].join
 end
 
