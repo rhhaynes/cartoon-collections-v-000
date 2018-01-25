8fc9347e100fdc72b60f3ed59c9cf24bf9bf5a47
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-25 14:39:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,6 @@ def long_planeteer_calls(arr)
   arr.any?{|n| n.length>4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
