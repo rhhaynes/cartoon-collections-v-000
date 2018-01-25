71e292eb87231428f5e7967c7783e09dd2f2ec68
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-25 14:34:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(array)
   array.each_with_index{|n,i| puts "#{i} #{n}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet()
+  
 end
 
 def long_planeteer_calls# code an argument here
